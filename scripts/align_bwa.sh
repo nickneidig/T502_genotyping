66c1a929ac1f8a38cfa00582a94bf47f286f406d
rtraborn <rtraborn@indiana.edu>
2018-02-06 01:49:44 -0500

    Completed WGS genotyping script.

--- scripts/align_bwa.sh
+++ scripts/align_bwa.sh
@@ -2,21 +2,22 @@
 
 #PBS -N WGS_genotyping_align_T502
 #PBS -k o
-#PBS -l nodes=1:ppn=16,vmem=80gb
-#PBS -l walltime=3:00:00
+#PBS -l nodes=1:ppn=16,vmem=40gb
+#PBS -l walltime=1:00:00
 #PBS -m abe
 
-module load star
+module load bwa
+module load samtools
 
-fileDir=/N/dc2/scratch/rtraborn/T502_fastqs/Genotyping_Ecoli
+fileDir=/N/dc2/scratch/rtraborn/T502_fastqs/Genotyping_Ecoli_new
 ####### Before running the script, please enter path to desired output directory, below ####
-WD=/N/dc2/scratch/rtraborn/T502_genotyping/
-#WD=<enter/path/to/T502_genotyping/>
+#WD=/N/dc2/scratch/rtraborn/T502_genotyping/
+WD=<enter/path/to/T502_genotyping/>
 fqDir=fastqs
 genomedir=${WD}/fasta
-KPgenome=pacificus_Hybrid2.fa
-ECIgenome=
-ECgenome=
+KPgenome=${genomedir}/K_pneumoniae_genome.fasta
+ECIgenome=${genomedir}/E_coli_genome.fasta
+ECgenome=${genomedir}/E_cloacae_genomic.fasta
 nThreads=16
 
 
@@ -32,44 +33,64 @@ if [ ! -d "$fqDir" ] ; then
 
 cd $fqDir
 
-ln -s ${fileDir}/GSF1659-EC-7_S17_R1_001.fastq.gz EC-7_S17_R1.fastq.gz 
-ln -s ${fileDir}/GSF1659-EC-7_S17_R2_001.fastq.gz  EC-7_S17_R2.fastq.gz 
-ln -s ${fileDir}/GSF1659-ECL-1_S15_R1_001.fastq.gz ECL-1_S15_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-ECL-1_S15_R2_001.fastq.gz ECL-1_S15_R2.fastq.gz
-ln -s ${fileDir}/GSF1659-ECL-4_S16_R1_001.fastq.gz ECL-4_S16_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-ECL-4_S16_R2_001.fastq.gz ECL-4_S16_R2.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-177_S18_R1_001.fastq.gz KP-177_S18_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-177_S18_R2_001.fastq.gz KP-177_S18_R2.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-49_S9_R1_001.fastq.gz KP-49_S9_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-49_S9_R2_001.fastq.gz KP-49_S9_R2.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-80_S10_R1_001.fastq.gz KP-80_S10_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-80_S10_R2_001.fastq.gz KP-80_S10_R2_001.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-83_S11_R1_001.fastq.gz KP-83_S11_R1.fastq.gz 
-ln -s ${fileDir}/GSF1659-KP-83_S11_R2_001.fastq.gz KP-83_S11_R2.fastq.gz 
-ln -s ${fileDir}/GSF1659-KP-84_S12_R1_001.fastq.gz KP-84_S12_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-84_S12_R2_001.fastq.gz KP-84_S12_R2.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-86_S14_R1_001.fastq.gz KP-86_S14_R1.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-86_S14_R2_001.fastq.gz KP-86_S14_R2.fastq.gz
-ln -s ${fileDir}/GSF1659-KP-85_S13_R1_001.fastq.gz KP-85_S13_R1.fastq.gz 
-ln -s ${fileDir}/GSF1659-KP-85_S13_R2_001.fastq.gz KP-85_S13_R2.fastq.gz
+ln -s ${fileDir}/GSF1659-EC-7_S17_R1_001.fastq EC-7_S17_R1.fastq
+ln -s ${fileDir}/GSF1659-EC-7_S17_R2_001.fastq  EC-7_S17_R2.fastq
+ln -s ${fileDir}/GSF1659-ECL-1_S15_R1_001.fastq ECL-1_S15_R1.fastq
+ln -s ${fileDir}/GSF1659-ECL-1_S15_R2_001.fastq ECL-1_S15_R2.fastq
+ln -s ${fileDir}/GSF1659-ECL-4_S16_R1_001.fastq ECL-4_S16_R1.fastq
+ln -s ${fileDir}/GSF1659-ECL-4_S16_R2_001.fastq ECL-4_S16_R2.fastq
+ln -s ${fileDir}/GSF1659-KP-177_S18_R1_001.fastq KP-177_S18_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-177_S18_R2_001.fastq KP-177_S18_R2.fastq
+ln -s ${fileDir}/GSF1659-KP-49_S9_R1_001.fastq KP-49_S9_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-49_S9_R2_001.fastq KP-49_S9_R2.fastq
+ln -s ${fileDir}/GSF1659-KP-80_S10_R1_001.fastq KP-80_S10_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-80_S10_R2_001.fastq KP-80_S10_R2_001.fastq
+ln -s ${fileDir}/GSF1659-KP-83_S11_R1_001.fastq KP-83_S11_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-83_S11_R2_001.fastq KP-83_S11_R2.fastq
+ln -s ${fileDir}/GSF1659-KP-84_S12_R1_001.fastq KP-84_S12_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-84_S12_R2_001.fastq KP-84_S12_R2.fastq
+ln -s ${fileDir}/GSF1659-KP-86_S14_R1_001.fastq KP-86_S14_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-86_S14_R2_001.fastq KP-86_S14_R2.fastq
+ln -s ${fileDir}/GSF1659-KP-85_S13_R1_001.fastq KP-85_S13_R1.fastq
+ln -s ${fileDir}/GSF1659-KP-85_S13_R2_001.fastq KP-85_S13_R2.fastq
 
 cd $WD
 
 echo "Retrieving the appropriate assembly files"
 
-source 0README
+#source 0README
 
 echo "Performing alignment on the bacterial files"
 
-readconfigfile $configfile
+cd $genomedir
+bwa index $KPgenome
+
+cd ${WD}/${fqDir}
+  for file1 in `ls KP-*_R1.fastq`; do
+    echo $file1
+    file2=$(basename $file1 _R1.fastq)_R2.fastq
+    echo "bwa mem $KPgenome $file1 $file2 | samtools view > $(basename $file1 _R1.fastq).bam"
+    bwa mem -t $nThreads $KPgenome $file1 $file2 | samtools view > $(basename $file1 _R1.fastq).bam
+  done
 
 cd $genomedir
+bwa index $ECIgenome
 
-  cd ${WD}/${fqDir}
-  for file1 in *.R1.fastq.gz; do
-    file2=$(basename $file1 .R1.fastq.gz).R2.fastq.gz
-    echo "STAR --runMode alignReads --runThreadN $numproc  ${STARalignReadsOptions}  --outSAMtype BAM SortedByCoordinate --outSAMorder Paired  --outFileNamePrefix $(basename $file1 _001.fastq.gz).STAR.  --genomeDir $genomedir  --readFilesIn ${file1} ${file2}"
+cd ${WD}/${fqDir}
+  for file1 in `ls ECL-*_R1.fastq`; do
+    file2=$(basename $file1 _R1.fastq)_R2.fastq
+    echo "bwa mem $KPgenome $file1 $file2 | samtools view > $(basename $file1 _R1.fastq).bam"
+    bwa mem -t $nThreads $ECLgenome $file1 $file2 | samtools view > $(basename $file1 _R1.fastq).bam
+  done
 
+cd $genomedir
+bwa index $ECgenome
+
+cd ${WD}/${fqDir}
+  for file1 in `ls EC-*_R1.fastq`; do
+    file2=$(basename $file1 _R1.fastq)_R2.fastq
+    echo "bwa mem $(basename KPgenome .fasta) $file1 $file2 | samtools view > $(basename $file1 _R1.fastq).bam"
+    bwa mem -t $nThreads $ECgenome $file1 $file2 | samtools view > $(basename $file1 _R1.fastq).bam
   done
 
   cd ${WD}
@@ -77,11 +98,11 @@ cd $genomedir
     mkdir alignments
   fi
   cd alignments
-  for file1 in ${WD}/$fqDir/*.STAR.*.bam ; do
+  for file1 in ${WD}/$fqDir/*.bam ; do
     echo $file1
     file2=$(basename $file1)
     echo $file2
-    ln -s $file1 ./${file2/.STAR.Aligned.sortedByCoord.out}
+    ln -s $file1 ./$file2
   done
   cd ..
 
@@ -89,8 +110,6 @@ cd $genomedir
   echo " Done with step 3 (read mapping)."
   echo ""
   echo "================================================================================"
-fi
-
 
 exit
 
